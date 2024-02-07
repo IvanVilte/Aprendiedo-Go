709841b2b950acb8129d229c79eef9b87189d328
Iván Vilte <ivanfvilte@gmail.com>
2024-02-07 19:27:52 -0300

    Return and handle an error terminado.

--- Greetings/greetings/greetings.go
+++ Greetings/greetings/greetings.go
@@ -1,10 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)		
 
-// Hola devuelve un saludo para la persona nombrada.
-func Hello(name string) string{
+// 'Hola' devuelve un saludo para la persona nombrada.
+func Hello(name string) (string, error){
+	
+	// Si no se ha dado ningún nombre, devuelve un error con un mensaje.
+	if name == "" {
+		return "", errors.New("Empty name.")
+	}
+	
+	// Si se ha recibido un nombre, devuelve un valor que incrusta el nombre en un mensaje de saludo.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	return message
+	return message, nil
 }
--- Greetings/hello/hello.go
+++ Greetings/hello/hello.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
-	
+	"log"
+
 	"example.com/greetings"
 )
 
 func main()  {
 	
-	message := greetings.Hello("Gladys")
+	//Establece las propiedades del Logger ('registrado') predefinido, incluyendo el prefijo
+	//de la entrada del log y una bandera para deshabilitar la impresión de la hora, el archivo fuente y el número de línea.
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	//Solicitar un mensaje de bienvenida.
+	message, err := greetings.Hello("")
+	
+	//Si se devuelve un error, imprimirlo en la consola y salir del programa.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Si no se devuelve ningún error, imprima el mensaje devuelto en la consola.
 	fmt.Println(message)
 }
